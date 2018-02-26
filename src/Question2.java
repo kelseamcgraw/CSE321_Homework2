e881bba2a650ad7bd50f33f28b6c721d352f8e2b
kelseamcgraw <mcgrawkr@miamioh.edu>
2018-02-26 12:18:32 -0500

    initial commit

---
+++ src/Question2.java
@@ -0,0 +1,4 @@
+
+public class Question2 {
+	//This is a test source code.
+}
---
+++ src/Question2Test.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Question2Test {
+	
+	//This is a test, test class.
+
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+
+}
